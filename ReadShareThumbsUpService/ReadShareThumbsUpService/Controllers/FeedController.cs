1427a71e0b89b66d41431f005cbcfacfedd13479
Michael Lang <mike_lang1010@yahoo.noroboemailsplease>
2012-07-01 19:06:32 -0500

    Corrected dependency injection of IFeedProvider

--- ReadShareThumbsUpService/ReadShareThumbsUpService/Controllers/FeedController.cs
+++ ReadShareThumbsUpService/ReadShareThumbsUpService/Controllers/FeedController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Web;
 using System.Web.Mvc;
+using Ninject;
 using ReadShareThumbsUpService.Provider;
 using SyncService.Misc;
 
@@ -8,21 +9,21 @@ namespace ReadShareThumbsUpService.Controllers
 {
     public class FeedController : Controller
     {
-
+        [Inject]
         public IFeedProvider FeedProvider { get; set; }
 
         [JsonpFilter]
         public ActionResult AddFeed(string userName, string feedUrl)
         {
             FeedProvider.AddFeed(userName, feedUrl);
-            return Json(new {response = "OK"});
+            return Json(new {response = "OK"}, JsonRequestBehavior.AllowGet);
         }
 
         [JsonpFilter]
         public ActionResult GetFeeds(string userName)
         {
             var feeds = FeedProvider.GetFeeds(userName);
-            return Json(feeds);
+            return Json(feeds, JsonRequestBehavior.AllowGet);
         }
     }
 }
